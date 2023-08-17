64fe52a92e9522c22b2d971896f056d014a04454
github-action-benchmark <github@users.noreply.github.com>
2023-08-17 09:22:16 +0000

    add Benchmark (cargo) benchmark result for b7dcdec24c04bddea9cc7814fa46836119cc580b

---
+++ bench/indexer/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1692264135970,
+  "repoUrl": "https://github.com/MystenLabs/sui",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "rvantonder@gmail.com",
+            "name": "Rijnard van Tonder",
+            "username": "rvantonder"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b7dcdec24c04bddea9cc7814fa46836119cc580b",
+          "message": "move: simplify dev only deps for source service (easy) (#13439)\n\n## Description \r\n\r\nAs in title\r\n\r\n## Test Plan \r\n\r\nHow did you test the new or updated feature?\r\n\r\nN/A,  semantics-preserving\r\n\r\n---\r\nIf your changes are not user-facing and not a breaking change, you can\r\nskip the following section. Otherwise, please indicate what changed, and\r\nthen add to the Release Notes section as highlighted during the release\r\nprocess.\r\n\r\n### Type of Change (Check all that apply)\r\n\r\n- [ ] protocol change\r\n- [ ] user-visible impact\r\n- [ ] breaking change for a client SDKs\r\n- [ ] breaking change for FNs (FN binary must upgrade)\r\n- [ ] breaking change for validators or node operators (must upgrade\r\nbinaries)\r\n- [ ] breaking change for on-chain data layout\r\n- [ ] necessitate either a data wipe or data migration\r\n\r\n### Release notes",
+          "timestamp": "2023-08-17T10:11:53+01:00",
+          "tree_id": "8b3293190f3726ddcc255ad83087fde4d5cc0bba",
+          "url": "https://github.com/MystenLabs/sui/commit/b7dcdec24c04bddea9cc7814fa46836119cc580b"
+        },
+        "date": 1692264134034,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "get_checkpoint",
+            "value": 341179,
+            "range": "± 19277",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
